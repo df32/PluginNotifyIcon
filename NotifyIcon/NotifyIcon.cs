5388e97df91b78dd570a515dcd291a8996880f4b
风飏 <suixin812@hotmail.com>
2014-10-16 08:39:44 +0800

    移入主要文件
    将工程文件移入库

---
+++ NotifyIcon/NotifyIcon.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+using Rainmeter;
+
+namespace Rainmeter.NotifyIconPlugin
+{
+	public partial class PluginNotifyIcon
+	{
+		internal partial class Measure
+		{
+			
+			NotifyIcon notifyIcon = new NotifyIcon();
+
+			internal void _SetIcon()
+			{
+				var icoUrl = rm.ReadPath("Icon", "");
+				if (!File.Exists(icoUrl))
+				{
+					API.Log(API.LogType.Error, "NotifyIcon: Invalid Icon: " + icoUrl);
+					return;
+				}
+				try
+				{
+					Icon ico = new Icon(icoUrl);
+					notifyIcon.Icon = ico;
+				}
+				catch (Exception ex)
+				{
+					API.Log(API.LogType.Error, "NotifyIcon: Invalid Icon: " + ex.Message);
+				}
+			}
+
+			internal void _SetText()
+			{
+				notifyIcon.Text = rm.ReadString("Text", rm.GetSkinName());
+			}
+
+			internal void _SetVisible(bool? visible)
+			{
+				if (visible == true)
+					notifyIcon.Visible = true;
+				else if (visible == false)
+					notifyIcon.Visible = false;
+				else
+					notifyIcon.Visible = !notifyIcon.Visible;
+			}
+
+			internal void _SetAction()
+			{
+				notifyIcon.Click += (sender, args) =>
+					{
+						_ExecuteAction("OnClickAction");
+					};
+
+				#region 一大波Action接近中
+				notifyIcon.DoubleClick += (sender, args) =>
+					{
+						_ExecuteAction("OnDoubleClickAction");
+					};
+
+				notifyIcon.MouseClick += (sender, args) =>
+				{
+					_ExecuteMouseAction("MouseClick", args);
+				};
+
+				notifyIcon.MouseDoubleClick += (sender, args) =>
+				{
+
+					_ExecuteMouseAction("MouseDoubleClick", args);
+				};
+
+
+				notifyIcon.MouseDown += (sender, args) =>
+				{
+					_ExecuteMouseAction("MouseDown", args);
+				};
+
+				notifyIcon.MouseMove += (sender, args) =>
+				{
+					_ExecuteAction("MouseMoveAction");
+				};
+
+				notifyIcon.MouseUp += (sender, args) =>
+				{
+					_ExecuteMouseAction("MouseUp", args);
+				};
+
+				notifyIcon.BalloonTipShown += (sender, args) =>
+				{
+					_ExecuteAction("BalloonTipShownAction");
+				};
+
+
+				notifyIcon.BalloonTipClicked += (sender, args) =>
+				{
+					_ExecuteAction("BalloonTipClickedAction");
+				};
+
+
+				notifyIcon.BalloonTipClosed += (sender, args) =>
+				{
+					_ExecuteAction("BalloonTipClosedAction");
+				};
+
+				#endregion
+			}
+
+			void _ExecuteAction(string option)
+			{
+				var action = rm.ReadString(option, "");
+				if (!string.IsNullOrEmpty(action))
+					API.Execute(skin, action);
+			}
+
+			void _ExecuteMouseAction(string option, MouseEventArgs args)
+			{
+				option += "Action";
+
+				if (args.Button == MouseButtons.Left)
+				{
+					option = ("Left" + option);
+				}
+				else if (args.Button == MouseButtons.Right)
+				{
+					option = ("Right" + option);
+				}
+				else if (args.Button == MouseButtons.Middle)
+				{
+					option = ("Middle" + option);
+				}
+				else
+				{
+					option = ("Other" + option);
+				}
+
+				var action = rm.ReadString(option, "");
+				if (!string.IsNullOrEmpty(action))
+				{
+					//action = action
+					//	.Replace("$Mouse:X$", args.X.ToString())
+					//	.Replace("$Mouse:Y$", args.Y.ToString());
+					//API.Log(API.LogType.Debug, action);
+					API.Execute(skin, action);
+				}
+			}
+			
+		}
+	}
+}
---
+++ NotifyIcon/Plugin.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+using Rainmeter;
+
+namespace Rainmeter.NotifyIconPlugin
+{
+	public partial class PluginNotifyIcon
+	{
+		internal partial class Measure
+		{
+			
+			bool _Initialized = false;
+			bool _DynamicText = false;
+			API rm;
+			IntPtr skin;
+			internal Measure()
+			{
+			}
+
+			internal void Finalize()
+			{
+				notifyIcon.Dispose();
+			}
+
+			internal void Reload(Rainmeter.API api, ref double maxValue)
+			{
+				rm = api;
+				skin = rm.GetSkin();
+				if (_DynamicText || !_Initialized)
+					_SetText();
+
+				if (_Initialized) return;
+				_Initialized = true;
+
+				_SetIcon();
+				
+				if (rm.ReadInt("VisibleOnLoad", 0) > 0)
+					_SetVisible(true);
+
+				_SetAction();
+
+				_DynamicText = rm.ReadInt("DynamicText", 0) > 0;
+			}
+
+
+			internal double Update()
+			{
+				return notifyIcon.Visible? 1.0: 0.0;
+			}
+
+			internal string GetString()
+			{
+			    return notifyIcon.Text;
+			}
+
+			internal void ExecuteBang(string args)
+			{
+				if (string.Compare(args, "Show", true) == 0)
+				{
+					_SetVisible(true);
+				}
+				else if (string.Compare(args, "Hide", true) == 0)
+				{
+					_SetVisible(false);
+				}
+				else if (string.Compare(args, "Toggle", true) == 0)
+				{
+					_SetVisible(null);
+				}
+				else if (string.Compare(args, "ReloadIcon", true) == 0)
+				{
+					_SetIcon();
+				}
+				else if (string.Compare(args, "ShowBalloonTip", true) == 0)
+				{
+					int timeout = rm.ReadInt("BalloonTipTimeOut", 1000);
+					string text = rm.ReadString("BalloonTipText", "");
+					string title = rm.ReadString("BalloonTipTitle", "");
+					string icon = rm.ReadString("BalloonTipIcon", "");
+					ToolTipIcon tipIcon = ToolTipIcon.None;
+
+					icon.ToLower();
+					if (icon.Contains("error"))
+						tipIcon = ToolTipIcon.Error;
+					else if (icon.Contains("info"))
+						tipIcon = ToolTipIcon.Info;
+					else if (icon.Contains("warning"))
+						tipIcon = ToolTipIcon.Warning;
+
+					try
+					{
+						notifyIcon.ShowBalloonTip(timeout, title, text, tipIcon);
+					}
+					catch (Exception ex)
+					{
+						API.Log(API.LogType.Error, "NotifyIcon: Unable to Show Balloon Tip: " + ex.Message);
+					}
+				}
+				else
+				{
+					notifyIcon.Text = args;
+				}
+			}
+
+			
+		}
+
+		public static class Plugin
+		{
+			[DllExport]
+			public unsafe static void Initialize(void** data, void* rm)
+			{
+				uint id = (uint)((void*)*data);
+				Measures.Add(id, new Measure());
+			}
+
+			[DllExport]
+			public unsafe static void Finalize(void* data)
+			{
+				uint id = (uint)data;
+				Measures[id].Finalize();
+				Measures.Remove(id);
+			}
+
+			[DllExport]
+			public unsafe static void Reload(void* data, void* rm, double* maxValue)
+			{
+				uint id = (uint)data;
+				Measures[id].Reload(new Rainmeter.API((IntPtr)rm), ref *maxValue);
+			}
+
+			[DllExport]
+			public unsafe static double Update(void* data)
+			{
+				uint id = (uint)data;
+				return Measures[id].Update();
+			}
+
+			[DllExport]
+			public unsafe static char* GetString(void* data)
+			{
+				uint id = (uint)data;
+				fixed (char* s = Measures[id].GetString()) return s;
+			}
+
+			[DllExport]
+			public unsafe static void ExecuteBang(void* data, char* args)
+			{
+				uint id = (uint)data;
+				Measures[id].ExecuteBang(new string(args));
+			}
+
+			internal static Dictionary<uint, Measure> Measures = new Dictionary<uint, Measure>();
+		}
+	}
+}
